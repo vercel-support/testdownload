0eb8dd89aabdd73f2cf3dbc11c5d60446281a4a2
nelakay <36554689+nelakay@users.noreply.github.com>
2023-01-26 22:51:40 +0000

    tenth step

---
+++ typings.d.ts
@@ -0,0 +1,11 @@
+export type Host = {
+    hostId: string;
+    username: string;
+    id: number;
+    full_name: string;
+    avatar_url: string;
+    is_host: boolean;
+    bio: string;
+    calendar_embed: string;
+    json: object;
+}
