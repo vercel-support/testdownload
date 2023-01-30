244119f3d4a0b0b7febd722416c8c2cedfcb424d
nelakay <36554689+nelakay@users.noreply.github.com>
2023-01-30 15:37:30 +0000

    bg-fix

--- app/hosts/[username]/page.tsx
+++ app/hosts/[username]/page.tsx
@@ -4,7 +4,6 @@ import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { supabase } from "../../../utils/supabase";
 import Script from "next/script";
 import Link from "next/link";
-import Router from 'next/navigation'
 
 export const revalidate = 60;
 
